1d249acc4d92cd6eaeb905921bfa41207e083c6e
samyush <samyushmaharjan@gmail.com>
2021-10-02 14:37:35 +0545

    api implementation errors

--- SchoolAppAsp.WebAPI/Startup.cs
+++ SchoolAppAsp.WebAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace SchoolAppAsp.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
